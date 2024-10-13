951d6bbe19212163ece2a9407fc76b4ed15c5bc9
Zhe Liu <zl376@cornell.edu>
2024-10-12 21:15:09 -0700

    Modify:   1. Fix bugs

--- src/inference.py
+++ src/inference.py
@@ -1,6 +1,7 @@
 import pyro
 from pyro.infer import SVI, Trace_ELBO
 from pyro.optim import Adam
+from pyro.infer.autoguide import AutoNormal
 
 def run_inference(model, x, y, num_iterations=1000):
     # Clear the param store in case we're rerunning this cell
@@ -9,7 +10,7 @@ def run_inference(model, x, y, num_iterations=1000):
     # Setup the optimizer and the inference algorithm
     adam_params = {"lr": 0.03, "betas": (0.90, 0.999)}
     optimizer = Adam(adam_params)
-    svi = SVI(model, pyro.infer.AutoDiagonalNormal(), Trace_ELBO(), optimizer)
+    svi = SVI(model, guide=AutoNormal(model), loss=Trace_ELBO(), optim=optimizer)
 
     # Do gradient steps
     for step in range(num_iterations):
